0ff37022cf8eab0742017ff30ef379a48a58d27e
Yupy <yupy17y@live.com>
2020-08-13 15:41:52 +0300

    Update bTemplate.php

--- btemplate/bTemplate.php
+++ btemplate/bTemplate.php
@@ -5,35 +5,39 @@
     License:        Please read the license.txt file.
     Last Updated:   11/27/2002
 \*--------------------------------------------------------------*/
-class bTemplate {
+class bTemplate
+{
     // Configuration variables
-    var $base_path = '';
-    var $reset_vars = TRUE;
+    public $base_path  = '';
+    public $reset_vars = true;
 
     // Delimeters for regular tags
-    var $ldelim = '<';
-    var $rdelim = ' />';
+    public $ldelim = '<';
+    public $rdelim = ' />';
 
     // Delimeters for beginnings of loops
-    var $BAldelim = '<';
-    var $BArdelim = '>';
+    public $BAldelim = '<';
+    public $BArdelim = '>';
 
     // Delimeters for ends of loops
-    var $EAldelim = '</';
-    var $EArdelim = '>';
+    public $EAldelim = '</';
+    public $EArdelim = '>';
 
-    // Internal variables
-    var $scalars = array();
-    var $arrays  = array();
-    var $carrays = array();
-    var $ifs     = array();
+	// Internal variables
+	var $scalars = array();
+	var $arrays  = array();
+	var $carrays = array();
+	var $ifs     = array();
 
     /*--------------------------------------------------------------*\
         Method: bTemplate()
         Simply sets the base path (if you don't set the default).
     \*--------------------------------------------------------------*/
-    function __construct($base_path = NULL, $reset_vars = TRUE) {
-        if($base_path) $this->base_path = $base_path;
+    public function __construct($base_path = null, $reset_vars = true)
+    {
+        if ($base_path) {
+            $this->base_path = $base_path;
+        }
         $this->reset_vars = $reset_vars;
     }
 
@@ -41,18 +45,20 @@ class bTemplate {
         Method: set()
         Sets all types of variables (scalar, loop, hash).
     \*--------------------------------------------------------------*/
-    function set($tag, $var, $if = NULL) {
-        if(is_array($var)) {
+    public function set($tag, $var, $if = null)
+    {
+        if (is_array($var)) {
             $this->arrays[$tag] = $var;
-            if($if) {
-                $result = $var ? TRUE : FALSE;
+            if ($if) {
+                $result = $var ? true : false;
                 $this->ifs[] = $tag;
                 $this->scalars[$tag] = $result;
             }
-        }
-        else {
+        } else {
             $this->scalars[$tag] = $var;
-            if($if) $this->ifs[] = $tag;
+            if ($if) {
+                $this->ifs[] = $tag;
+            }
         }
     }
 
@@ -60,28 +66,41 @@ class bTemplate {
         Method: set_cloop()
         Sets a cloop (case loop).
     \*--------------------------------------------------------------*/
-    function set_cloop($tag, $array, $cases) {
-        $this->carrays[$tag] = array(
+    public function set_cloop($tag, $array, $cases)
+    {
+        $this->carrays[$tag] = [
             'array' => $array,
-            'cases' => $cases);
+            'cases' => $cases];
     }
 
     /*--------------------------------------------------------------*\
         Method: reset_vars()
         Resets the template variables.
     \*--------------------------------------------------------------*/
-    function reset_vars($scalars, $arrays, $carrays, $ifs) {
-        if($scalars) $this->scalars = array();
-        if($arrays)  $this->arrays  = array();
-        if($carrays) $this->carrays = array();
-        if($ifs)     $this->ifs     = array();
-    }
+	function reset_vars($scalars, $arrays, $carrays, $ifs) {
+		if ($scalars) {
+            $this->scalars = array();
+        }
+
+		if ($arrays) {
+            $this->arrays = array();
+        }
+
+		if ($carrays) {
+            $this->carrays = array();
+        }
+
+		if ($ifs) {
+            $this->ifs = array();
+        }
+	}
 
     /*--------------------------------------------------------------*\
         Method: get_tags()
         Formats the tags & returns a two-element array.
     \*--------------------------------------------------------------*/
-    function get_tags($tag, $directive) {
+    public function get_tags($tag, $directive)
+    {
         $tags['b'] = $this->BAldelim . $directive . $tag . $this->BArdelim;
         $tags['e'] = $this->EAldelim . $directive . $tag . $this->EArdelim;
         return $tags;
@@ -91,7 +110,8 @@ class bTemplate {
         Method: get_tag()
         Formats a tag for a scalar.
     \*--------------------------------------------------------------*/
-    function get_tag($tag) {
+    public function get_tag($tag)
+    {
         return $this->ldelim . 'tag:' . $tag . $this->rdelim;
     }
 
@@ -99,10 +119,10 @@ class bTemplate {
         Method: get_statement()
         Extracts a portion of a template.
     \*--------------------------------------------------------------*/
-    function get_statement($t, &$contents) {
+    public function get_statement($t, &$contents)
+    {
         // Locate the statement
-        $tag_length = strlen((string)$t['b']);
-
+        $tag_length = strlen($t['b']);
         if (!empty($t['b'])) {
             $fpos = strpos($contents, (string)$t['b']) + $tag_length;
         }
@@ -118,31 +138,34 @@ class bTemplate {
         Method: parse()
         Parses all variables into the template.
     \*--------------------------------------------------------------*/
-    function parse($contents) {
+    public function parse($contents)
+    {
         // Process the ifs
-        if(!empty($this->ifs)) {
-            foreach($this->ifs as $value) {
+        if (!empty($this->ifs)) {
+            foreach ($this->ifs as $value) {
                 $contents = $this->parse_if($value, $contents);
             }
         }
 
         // Process the scalars
-        foreach($this->scalars as $key => $value) {
+        foreach ($this->scalars as $key => $value) {
             $contents = str_replace($this->get_tag($key), $value, $contents);
         }
 
         // Process the arrays
-        foreach($this->arrays as $key => $array) {
+        foreach ($this->arrays as $key => $array) {
             $contents = $this->parse_loop($key, $array, $contents);
         }
 
         // Process the carrays
-        foreach($this->carrays as $key => $array) {
+        foreach ($this->carrays as $key => $array) {
             $contents = $this->parse_cloop($key, $array, $contents);
         }
 
         // Reset the arrays
-        if($this->reset_vars) $this->reset_vars(FALSE, TRUE, TRUE, FALSE);
+        if ($this->reset_vars) {
+            $this->reset_vars(false, true, true, false);
+        }
 
         // Return the contents
         return $contents;
@@ -154,7 +177,8 @@ class bTemplate {
         the <else:tag> tag doesn't conform to convention, so some
         things have to be done manually.
     \*--------------------------------------------------------------*/
-    function parse_if($tag, $contents) {
+    public function parse_if($tag, $contents)
+    {
         // Get the tags
         $t = $this->get_tags($tag, 'if:');
         
@@ -162,19 +186,18 @@ class bTemplate {
         $entire_statement = $this->get_statement($t, $contents);
         
         // Get the else tag
-        $tags['b'] = NULL;
-        $tags['e'] = $this->BAldelim . 'else:' . $tag . $this->BArdelim;        
+        $tags['b'] = null;
+        $tags['e'] = $this->BAldelim . 'else:' . $tag . $this->BArdelim;
         
         // See if there's an else statement
-        if(($else = strpos($entire_statement, $tags['e']))) {       
+        if (($else = strpos($entire_statement, $tags['e']))) {
             // Get the if statement
             $if = $this->get_statement($tags, $entire_statement);
         
             // Get the else statement
             $else = substr($entire_statement, $else + strlen($tags['e']));
-        }
-        else {
-            $else = NULL;
+        } else {
+            $else = null;
             $if = $entire_statement;
         }
         
@@ -189,36 +212,36 @@ class bTemplate {
         Method: parse_loop()
         Parses a loop (recursive function).
     \*--------------------------------------------------------------*/
-    function parse_loop($tag, $array, $contents) {
+    public function parse_loop($tag, $array, $contents)
+    {
         // Get the tags & loop
         $t = $this->get_tags($tag, 'loop:');
         $loop = $this->get_statement($t, $contents);
-        $parsed = NULL;
+        $parsed = null;
 
         // Process the loop
-        foreach($array as $key => $value) {
-            if(is_numeric($key) && is_array($value)) {
+        foreach ($array as $key => $value) {
+            if (is_numeric($key) && is_array($value)) {
                 $i = $loop;
-                foreach($value as $key2 => $value2) {
-                    if(!is_array($value2)) {
+                foreach ($value as $key2 => $value2) {
+                    if (!is_array($value2)) {
                         // Replace associative array tags
                         $i = str_replace($this->get_tag($tag . '[].' . $key2), $value2, $i);
-                    }
-                    else {
+                    } else {
                         // Check to see if it's a nested loop
                         $i = $this->parse_loop($tag . '[].' . $key2, $value2, $i);
                     }
                 }
-            }
-            elseif(is_string($key) && !is_array($value)) {
+            } elseif (is_string($key) && !is_array($value)) {
                 $contents = str_replace($this->get_tag($tag . '.' . $key), $value, $contents);
-            }
-            elseif(!is_array($value)) {
+            } elseif (!is_array($value)) {
                 $i = str_replace($this->get_tag($tag . '[]'), $value, $loop);
             }
 
             // Add the parsed iteration
-            if(isset($i)) $parsed .= rtrim($i);
+            if (isset($i)) {
+                $parsed .= rtrim($i);
+            }
         }
 
         // Parse & return the final loop
@@ -229,7 +252,8 @@ class bTemplate {
         Method: parse_cloop()
         Parses a cloop (case loop) (recursive function).
     \*--------------------------------------------------------------*/
-    function parse_cloop($tag, $array, $contents) {
+    public function parse_cloop($tag, $array, $contents)
+    {
         // Get the tags & loop
         $t = $this->get_tags($tag, 'cloop:');
         $loop = $this->get_statement($t, $contents);
@@ -237,25 +261,28 @@ class bTemplate {
         // Set up the cases
         $array['cases'][] = 'default';
         $case_content = array();
-        $parsed = NULL;
+        $parsed = null;
 
         // Get the case strings
-        foreach($array['cases'] as $case) {
+        foreach ($array['cases'] as $case) {
             $ctags[$case] = $this->get_tags($case, 'case:');
             $case_content[$case] = $this->get_statement($ctags[$case], $loop);
         }
 
         // Process the loop
-        foreach($array['array'] as $key => $value) {
-            if(is_numeric($key) && is_array($value)) {
+        foreach ($array['array'] as $key => $value) {
+            if (is_numeric($key) && is_array($value)) {
                 // Set up the cases
-                if(isset($value['case'])) $current_case = $value['case'];
-                else $current_case = 'default';
+                if (isset($value['case'])) {
+                    $current_case = $value['case'];
+                } else {
+                    $current_case = 'default';
+                }
                 unset($value['case']);
                 $i = $case_content[$current_case];
 
                 // Loop through each value
-                foreach($value as $key2 => $value2) {
+                foreach ($value as $key2 => $value2) {
                     $i = str_replace($this->get_tag($tag . '[].' . $key2), $value2, $i);
                 }
             }
@@ -272,13 +299,16 @@ class bTemplate {
         Method: fetch()
         Returns the parsed contents of the specified template.
     \*--------------------------------------------------------------*/
-    function fetch($file_name) {
+    public function fetch($file_name)
+    {
         // Prepare the path
         $file = $this->base_path . $file_name;
 
         // Open the file
         $fp = fopen($file, 'rb');
-        if(!$fp) return FALSE;
+        if (!$fp) {
+            return false;
+        }
         
         // Read the file
         $contents = fread($fp, filesize($file));
